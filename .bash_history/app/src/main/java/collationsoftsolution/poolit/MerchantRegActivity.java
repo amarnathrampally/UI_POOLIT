8937030fa61b52e2c186198d1e2d8bddb7ba4639
sai manish <sai manish>
2016-06-08 17:41:52 +0530

    C:/Program Files/Git/made login page and user and merchant registration pages

---
+++ .bash_history/app/src/main/java/collationsoftsolution/poolit/MerchantRegActivity.java
@@ -0,0 +1,13 @@
+package collationsoftsolution.poolit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MerchantRegActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_merchant_reg);
+    }
+}
