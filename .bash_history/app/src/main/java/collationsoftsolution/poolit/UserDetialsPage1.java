6a41ff6f5ddd5ed3b55d2358a1c20fbc02f246f8
sai manish <sai manish>
2016-06-08 20:07:09 +0530

    Added user details page 1

--- .bash_history/app/src/main/java/collationsoftsolution/poolit/UserDetialsPage1.java
+++ .bash_history/app/src/main/java/collationsoftsolution/poolit/UserDetialsPage1.java
@@ -3,14 +3,11 @@ package collationsoftsolution.poolit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class StartUpActivity extends AppCompatActivity {
+public class UserDetialsPage1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_up);
-
-
-
+        setContentView(R.layout.activity_user_detials_page1);
     }
 }
